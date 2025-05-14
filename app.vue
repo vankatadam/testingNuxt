0617f32ea732f8758eb49dd622264b7e34392321
Adam Vankat <vankatadam@gmail.com>
2025-05-14 20:12:42 +0200

    state

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
 <template>
   <div>
+    <button @click="count++">You clicked me {{ count }} times.</button>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
